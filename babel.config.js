861f56b6fff2f73cccd71844c918c70a7bd3264d
Inesss <ines.chanbi@enicar.ucar.tn>
2024-05-20 16:09:16 +0100

    On branch main

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
+
+  const presets = ['babel-preset-expo'];
+  const plugins = [['module:react-native-dotenv', { moduleName: '@env' }]];
+
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    presets,
+    plugins
   };
 };
